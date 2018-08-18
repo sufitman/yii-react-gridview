b03ed00bb6bc6a3e4a7ca555116654790ddf8e77
Timur <sufitman@gmail.com>
2018-08-19 02:10:43 +0300

    update webpack, minify dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,69 @@
-var webpack = require('webpack')
+var webpack = require('webpack');
 
 module.exports = [{
-    context: `${__dirname}/src`,
-    entry: {
-        javascript: './GridView.js'
-    },
-    output: {
-        filename: 'yii-react-gridview.js',
-        path: `${__dirname}/dist`,
-        library: 'YiiReactGridView',
-        libraryTarget: 'umd'
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            React: 'react'
-        })
-    ],
-    externals: [
-        {
-            react: {
-                root: 'React',
-                commonjs2: 'react',
-                commonjs: 'react',
-                amd: 'react',
-                umd: 'react',
-            },
-            'react-dom': {
-                root: 'ReactDOM',
-                commonjs2: 'react-dom',
-                commonjs: 'react-dom',
-                amd: 'react-dom',
-                umd: 'react-dom',
-            },
-        }
-    ],
-    module: {
-        loaders: [
-            {
-                test: /\.js(x?)$/,
-                include: /src/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2017', 'stage-2', 'react']
-                }
-            }
-        ]
+  context: `${__dirname}/src`,
+  entry: {
+    javascript: './GridView.js'
+  },
+  output: {
+    filename: 'yii-react-gridview.js',
+    path: `${__dirname}/dist`,
+    library: 'YiiReactGridView',
+    libraryTarget: 'umd'
+  },
+  optimization: {
+    minimize: true
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      React: 'react'
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ],
+  externals: [
+    {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react',
+        umd: 'react',
+      },
+      'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom',
+        umd: 'react-dom',
+      },
+      'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom',
+        umd: 'react-dom',
+      },
+      'prop-types': {
+        root: 'PropTypes',
+        commonjs2: 'prop-types',
+        commonjs: 'prop-types',
+        amd: 'prop-types',
+        umd: 'prop-types',
+      },
     }
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js(x?)$/,
+        include: /src/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2017', 'stage-2', 'react']
+        }
+      }
+    ]
+  }
 }];
