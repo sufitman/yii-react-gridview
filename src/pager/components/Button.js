c8d544c3226559d00130a1b65fd7e42c5a891248
Timur <sufitman@gmail.com>
2019-03-20 14:16:31 +0300

    fix pager buttons class names

--- src/pager/components/Button.js
+++ src/pager/components/Button.js
@@ -7,14 +7,12 @@ type PagerButtonProps = {
   disabled: boolean,
   active: boolean,
   page: number,
-  className: string | Array<string>,
   content: React.Node,
   onPageButtonClick: PageButtonClick,
 } & GeneralButtonOptions;
 
 export default class Button extends React.PureComponent<PagerButtonProps> {
   static defaultProps = {
-    className: [],
     active: false,
     disabled: false,
   };
@@ -31,39 +29,29 @@ export default class Button extends React.PureComponent<PagerButtonProps> {
         (data: ButtonContextData) => {
           let Tag = data.pageTag;
           let LinkTag = 'a';
-          let options = {};
-          options.className = this.props.className;
-
-          if (typeof options.className === 'string') {
-            options.className = options.className.split(' ');
-          }
+          const className = [];
 
           if (this.props.isNextPage) {
-            options.className.push(data.nextPageCssClass);
+            className.push(data.nextPageCssClass);
           }
           if (this.props.isPrevPage) {
-            options.className.push(data.prevPageCssClass);
+            className.push(data.prevPageCssClass);
           }
           if (this.props.isLastPage) {
-            options.className.push(data.lastPageCssClass);
+            className.push(data.lastPageCssClass);
           }
           if (this.props.isFirstPage) {
-            options.className.push(data.firstPageCssClass);
+            className.push(data.firstPageCssClass);
           }
           if (this.props.active) {
-            options.className.push(data.activePageCssClass);
+            className.push(data.activePageCssClass);
           }
           if (this.props.disabled) {
-            options.className.push(data.disabledPageCssClass);
+            className.push(data.disabledPageCssClass);
             LinkTag = 'span';
           }
-          if (options.className.length) {
-            options.className = options.className.join(' ');
-          } else {
-            delete options.className;
-          }
           return <Tag>
-            <LinkTag onClick={ this.clickTag } { ...options }>
+            <LinkTag onClick={ this.clickTag } className={className.join(' ')}>
               { this.props.content }
             </LinkTag>
           </Tag>;
